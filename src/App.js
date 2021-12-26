cc75eaf5941d40b1709068970669970b9d14c396
Skdev <alexandre.mtimet@gmail.com>
2021-12-26 02:40:29 +0100

    Application integreation of module View & Button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import View from './composant/view';
+import Button from './composant/view';
 
 function App() {
   return (
     <div className="App">
       <div className="grid">
         <div className="box">
-          <View/>
+          <Button></Button>
+          
         </div>
       </div>
     </div>
