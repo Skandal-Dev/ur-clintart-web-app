9ea9f989588bbb35d3cb6c58cf1e5130b39e318d
Skdev <alexandre.mtimet@gmail.com>
2021-12-28 05:17:56 +0100

    Refactoring component & integration of Editor Component

--- src/composant/view.js
+++ src/composant/view.js
@@ -5,16 +5,20 @@ import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
 import download,{ data, strFileName, strMimeType } from "downloadjs";
 import { useRef } from "react";
 import domtoimage from "dom-to-image";
+import Editor from "./editor";
+import { useState } from 'react';
 
-function exportToPng(dom) {
+
+
+function exportToPng(dom, name) {
     domtoimage
       .toPng(dom)
       .then(function (dataUrl) {
         var img = new Image();
         img.src = dataUrl;
-        document.body.appendChild(img);
+        //document.body.appendChild(img);
         var link = document.createElement('a');
-        link.download = 'card.png';
+        link.download = name+'.png';
         link.href = dataUrl;
         link.click();
       })
@@ -26,61 +30,125 @@ function exportToPng(dom) {
 
   export  default function Button() {
     const container = useRef(null);
-   
+    const [nameCard, setNameCard] = useState("Card pute");
+    const [damage, setDamage] = useState(5);
+    const [power, setPower] = useState(4);
+    const [ability, setAbility] = useState("ability");
+    const [bonus, setBonus] = useState("bonus");
+    const [lvl, setLvl] = useState(4);
+    const [rarity, setRarity] = useState('r');
+    const [clan, setClan] = useState('BERZERK');
+    const [img, setImg] = useState('');
+    const [prismatic, setPrismatic] = useState('');
+
+ 
     return (
       <div className="button">
-        <View refference={container}></View>
-        <button className="btn btn-primary font-ur p-4 btn-lg play-button d-none d-md-inline-block position-relative l-height-5" 
-                onClick={() => exportToPng(container.current)}> Download </button>
+
+        <div className="flex">
+
+        <View refference={container} 
+        name_card={nameCard} damage={damage} power={power} ability={ability} 
+        bonus={bonus} lvl={lvl} clan={clan} rarity={rarity} 
+        img={img} prismatic={prismatic}>
+        </View>
+
+        <Editor name={nameCard} damage={damage} power={power} ability={ability} 
+        bonus={bonus} lvl={lvl} clan={clan} rarity={rarity} 
+        img={img} prismatic={prismatic}
+        
+        setNameCard={setNameCard} setDamage={setDamage} setPower={setPower} setAbility={setAbility} 
+        setBonus={setBonus} setLvl={setLvl} setLogo={setClan} setRarity={setRarity}
+        setImg={setImg} setPrismatic={setPrismatic}>
+        
+
+        </Editor>
+
+
+        </div>
+
+        <button className="btn btn-primary font-ur p-4 btn-lg play-button d-none d-md-inline-block  l-height-5" 
+                onClick={() => exportToPng(container.current, nameCard)}> Download </button>
       </div>
     );
   }
 
+
+export function Star(){
+
+    return (
+        <img className="card-star-on" src="https://s.acdn.ur-img.com/img/v3/card/icon-star-on.png" alt="star"/>
+    );
+}
+
+
+export function Stars(lvl){
+    console.log(lvl.lvl)
+    let arr = [];
+    for (let index = 0; index < lvl.lvl; index++) {
+        arr.push(<Star></Star>);
+    }
+
+    return (
+        <div>
+            {arr}
+        </div>
+    );
+}
+
 class View extends React.Component{
 
-    
+    // Receive props
    
     render(){
         
+       
+        let lvl = this.props.lvl;
+        let prism = "";
+        if (this.props.prismatic)
+            prism = "prismatic";
+        else
+            prism = "";
 
         return (
             <div  id="view">
 
-         
-            <div id="" className="ur-card card-c" ref={this.props.refference}>
+           
+            <div id="" className={`ur-card card-${this.props.rarity} ${prism}`} ref={this.props.refference}>
                 
                 <img className="card-picture" src="https://s.acdn.ur-img.com/urimages/perso/ALLSTARS/ALLSTARS_FRANK_N1_HD_188.png" data-src="https://s.acdn.ur-img.com/urimages/perso/ALLSTARS/ALLSTARS_FRANK_N1_HD_188.png" alt="picture"/>
-                    <div className="card-layer layer-c"></div>
+                    <div className={`card-layer layer-${this.props.rarity} ${prism}`}></div>
                     <a className="card-link"></a>
-                        <div className="card-top card-top-c">
+                        <div className={`card-top card-top-${this.props.rarity} ${prism}`}>
                             <a className="card-clan-link">
-                                <img className="card-clan img-fluid" src="https://s.acdn.ur-img.com/urimages/clan/ALLSTARS_42.png" alt="All Stars"/>
+                          
+                                <img className="card-clan img-fluid" src={`https://s.acdn.ur-img.com/urimages/clan/${this.props.clan}_42.png`} alt="All Stars"/>
                             </a>
-                        <span className="h6 card-name m-0">Frank</span>
+                        <span className="h6 card-name m-0">{this.props.name_card}</span>
                         </div>
                     <div className="card-bottom">
                         <div className="progress card-progress">
                             <div className="progress-bar" role="progressbar" style={{width: "100%"}}></div>
                         <div className="card-stars">
-                            <img className="card-star-on" src="https://s.acdn.ur-img.com/img/v3/card/icon-star-on.png" alt="star"/>
-                                <img className="card-star-off" src="https://s.acdn.ur-img.com/img/v3/card/icon-star-off.png" alt="star"/>
+                                
+                                <Stars lvl={this.props.lvl}></Stars>
+                            
                             </div>
                             </div>
-                            <div className="h5 card-power m-0">2</div>
-                            <div className="h5 card-damage m-0">1</div>
-                            <div className="card-ability text-black">Ability at level 
-                                <img className="card-star-on" src="https://s.acdn.ur-img.com/img/v3/card/icon-star-on.png" alt="star"/>
-                                <img className="card-star-on" src="https://s.acdn.ur-img.com/img/v3/card/icon-star-on.png" alt="star"/>
+                            <div className="h5 card-power m-0">{this.props.power}</div>
+                            <div className="h5 card-damage m-0">{this.props.damage}</div>
+                            <div className="card-ability text-black">
+                                {this.props.ability}
                             </div>
+                            <div className="card-bonus text-black">{this.props.bonus}</div>
 
                         </div>
-                        <div className="card-bonus text-black">-2 Opp Power, Min 1</div>
 
                         
                     </div>
 
 
-
+                    
             </div>
                 
             
