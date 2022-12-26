7e8b556953e72a8ca649d1f5dbf6167e3bf3cd14
Alexandre Mtimet <alexandre.mtimet@gmail.com>
2022-12-27 00:56:50 +0100

    add absolute path

--- src/composant/view.js
+++ src/composant/view.js
@@ -27,7 +27,7 @@ function resizeCanvas(canvas, newHeight, newWidth)
 
 
 function Share(dom, name){
-  axios.post("http://urban-rivals-clint-artweb.netlify.app/add", {
+  axios.post("https://urban-rivals-clint-artweb.netlify.app/add", {
     nameCard: dom.nameCard,
     lvl: dom.lvl,
     rarity: dom.rarity,
