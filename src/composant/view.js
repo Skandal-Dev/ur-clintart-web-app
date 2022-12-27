3f044c94b2241916e1ce562cf336c2c49f5391cd
Alexandre Mtimet <alexandre.mtimet@gmail.com>
2022-12-27 20:51:12 +0100

    update path for deploy

--- src/composant/view.js
+++ src/composant/view.js
@@ -27,7 +27,7 @@ function resizeCanvas(canvas, newHeight, newWidth)
 
 
 function Share(dom, name){
-  axios.post("/add", {
+  axios.post("localhost:3030/add", {
     nameCard: dom.nameCard,
     lvl: dom.lvl,
     rarity: dom.rarity,
